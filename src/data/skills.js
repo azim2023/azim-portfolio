47c09ec5253e30bec4e626329ea237d56eb19a5a
azim2023 <129534274+azim2023@users.noreply.github.com>
2025-08-22 11:19:05 +0200

    update

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,5 +3,5 @@ export const skills = {
   web: ["React", "Node.js", "Express", "REST API", "Webb-arkitektur"],
   methodologies: ["Agila metoder (Scrum)", "DevOps", "CI/CD", "Objektorienterad design", "Designmönster", "Testdriven utveckling (TDD)"],
   //tools: ["Git", "Docker", "Xcode", "Android Studio", "Linux", "Visual Studio Code"],
-  other: ["Datastrukturer & Algoritmer", "Databashantering", "Nätverksprogrammering", "Operativsystem", "Maskininlärning", "Gruppdynamik & Projektledning"]
+  other: ["Datastrukturer & Algoritmer", "Databashantering", "Nätverksprogrammering", "Operativsystem", "Gruppdynamik & Projektledning"]
 };
